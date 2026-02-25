36264267cad05f1f91fa844ef21a1f1a874e6300
mossann <yuki.iyotani@nulab.com>
2026-02-25 17:12:31 +0900

    Create State management Settings

---
+++ state-mgmt.tf
@@ -0,0 +1,20 @@
+#state管理用ファイル。Stateの記録をS3に吐き出す
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  # Stateファイルの保存先（事前に作成したバケット名を指定）
+  backend "s3" {
+    bucket         = "mtfstate-20260225"
+    key            = "network/terraform.tfstate"
+    region         = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
